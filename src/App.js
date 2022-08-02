1fb150765f4a02b2ac6f26f830ce57e0e988ce9f
payalrajyaguru-19 <payalrajyaguru19@gmail.com>
2022-08-02 19:07:37 +0530

    ]

--- src/App.js
+++ src/App.js
@@ -3,6 +3,43 @@ import './App.css';
 
 function App()
 {
+          //array
+
+  // let arr = [11,25,37,42,67];
+  // let arr1 = [99,100];
+
+  // let newArr = arr.concate (arr1);
+  // console.log(arr);
+
+  // let arr = [19,35,68,65,89];
+  // let ans = arr.some ((a) => a > 40);    //true
+  // console.log(ans);
+  
+  // let arr,toString= ['29,58,69,77,89'];
+  //  console.log(arr.toString);
+
+  // let arr = [11,45,66,56,88]
+  // let ans = arr.find ((a) => a > 60);    //66
+  // console.log(ans);
+ 
+  // let arr = [11,25,37,42,67];
+  // arr.splice(2 ,0, 35)      / [11, 25, 35, 37, 42, 67]
+  // console.log(arr);
+
+  //  let arr = [12,25,37,52,77];
+  //  arr.splice(2 ,1)      // [12, 25, 52, 77]
+  //  console.log(arr);
+
+      //Object
+
+  // let obj ={
+  //   id : 6500 ,
+  //   name : "payal"        //6500 'payal'
+  // }    
+  // console.log(obj.id ,obj['name']);
+
+
+
   // let x;
   // x=5;
   // console.log(x);
